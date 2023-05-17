d48e9ad1c546f9aa6a5e4b2f91b9019b39491c68
ana clara <anaclaraLovely2@gmail.com>
2023-05-17 19:27:02 -0400

    mensagem

---
+++ script/tarefa.js
@@ -0,0 +1,6 @@
+var hor = parseInt(prompt('Informe a(s) hora(s): '));
+var min = parseInt(prompt('Informe o(s) minuto(s): '));
+
+var tmin = (hor * 60) + min;
+
+document.write("é equivalente à:" + tmin + " minutos.");
