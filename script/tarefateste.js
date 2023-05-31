b83d3353bb2acd73fb14b856998bc1f4c2f5e5f1
ana clara <anaclaraLovely2@gmail.com>
2023-05-30 22:09:46 -0400

    mensagem

--- script/tarefateste.js
+++ script/tarefateste.js
@@ -2,5 +2,5 @@ var n1 = parseInt(prompt('Informe o primeiro valor: '));
     var n2 = parseInt(prompt('Informe o segundo valor: '));
 
     if (n1 > n2){
-        print("o primeiro é maior que o segundo")
+        print("o primeiro é maior que o segundo");
     }
